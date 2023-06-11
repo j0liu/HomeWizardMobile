96040f28da254cdf2c7f1eaa746f868c6c4462a1
Luciano <lucianoneimark@gmail.com>
2023-06-11 16:13:35 -0300

    Door view with wrong icons

---
+++ app/src/main/java/ar/edu/itba/homewizard/ui/devices/DoorUiState.kt
@@ -0,0 +1,8 @@
+package ar.edu.itba.homewizard.ui.devices
+
+
+
+data class DoorUiState (
+    val locked : Boolean = false,
+    val closed : Boolean = false
+)
---
+++ app/src/main/java/ar/edu/itba/homewizard/viewmodels/DoorViewModel.kt
@@ -0,0 +1,25 @@
+package ar.edu.itba.homewizard.viewmodels
+
+import androidx.lifecycle.ViewModel
+import ar.edu.itba.homewizard.ui.devices.DevicesUiState
+import ar.edu.itba.homewizard.ui.devices.DoorUiState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class DoorViewModel : ViewModel() {
+
+    private val _uiState = MutableStateFlow(DoorUiState())
+    val uiState : StateFlow<DoorUiState> = _uiState.asStateFlow()
+
+    fun toggleLock(){
+        _uiState.update{ oldState -> DoorUiState(!oldState.locked, oldState.closed) }
+    }
+
+
+    fun toggleClose(){
+        _uiState.update{ oldState -> DoorUiState(oldState.locked, !oldState.closed) }
+    }
+
+}
