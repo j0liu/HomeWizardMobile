9d96406bad6084d9b25f9ff85fbdf21662acc539
Luciano <lucianoneimark@gmail.com>
2023-06-15 11:26:11 -0300

    Removing unused import

--- app/src/main/java/ar/edu/itba/homewizard/ui/theme/Theme.kt
+++ app/src/main/java/ar/edu/itba/homewizard/ui/theme/Theme.kt
@@ -5,7 +5,6 @@ import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
-import androidx.compose.material3.ColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
